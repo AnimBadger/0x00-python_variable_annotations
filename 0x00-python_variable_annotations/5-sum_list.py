6b084574a717be2c05dde9f1ab845bbed110ee09
AnimBadger <anim.ansah.stephen@gmail.com>
2023-10-07 00:03:56 +0000

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''task 5 module'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''function that takes a list of floating numbers and return the sum'''
+    return float(sum(input_list))
