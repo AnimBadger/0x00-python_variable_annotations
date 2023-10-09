e4754814e89f81528786c52f9647756baad84acc
AnimBadger <anim.ansah.stephen@gmail.com>
2023-10-09 17:03:53 +0000

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+# /usr/bin/env python3
+'''task 8 module'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''function that takes a float and return a function'''
+    return lambda x: x * multiplier
